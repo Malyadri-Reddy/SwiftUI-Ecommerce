723ab8765711eaf453a493e5c2ced506ccc25fe8
Malyadri <malyadri.marthula@abbott.com>
2025-03-06 20:05:40 +0530

    Initial Commit

---
+++ SwiftUI-Ecommerce/SwiftUI_EcommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EcommerceApp.swift
+//  SwiftUI-Ecommerce
+//
+//  Created by Malyadri Reddy on 06/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EcommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI-EcommerceTests/SwiftUI_EcommerceTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EcommerceTests.swift
+//  SwiftUI-EcommerceTests
+//
+//  Created by Malyadri Reddy on 06/03/25.
+//
+
+import Testing
+@testable import SwiftUI_Ecommerce
+
+struct SwiftUI_EcommerceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
